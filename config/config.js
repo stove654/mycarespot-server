887fac47f18f55ca93dd2263c9a35cd2d34b763a
Loi Hoang <loihoang@Lois-MacBook-Pro.local>
2017-11-15 17:54:04 +0700

    update new onesignal

--- config/config.js
+++ config/config.js
@@ -4,8 +4,8 @@ module.exports = {
     userRoles: ['guest', 'user', 'admin'],
     accountKitUrl: 'https://graph.accountkit.com/v1.2/access_token',
     accessToken: '6f963fe06ebc888cdd74d55fd159f6ab',
-    oneSignalKey: 'N2VhNmYwZDUtMDMyMC00MDIyLWE5YzMtYTM1MDBmYzQyZDQ0',
-    oneSignalAppId: '0aefe444-ad09-494a-b457-2d9febdbf78e',
+    oneSignalKey: 'NzU0Y2I4NmMtNDY4Yi00OTFiLTkwOTUtN2JmYmM3YjY4NTU3',
+    oneSignalAppId: '8a13c6fc-fa4a-4158-8938-b281c2832756',
     accountSid: 'AC81153fd1c273e81456da3a41cdf8749a',
     authToken: '3c27528a9e96fe00a470c9fcea636d12',
     yourPhone: '+16502032489'
