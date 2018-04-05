dd4f40e77ec5e2f9e3b80152fa7d9e5db299249d
Loi Hoang <imstart25@gmail.com>
2018-04-05 22:41:30 +0700

    update

--- config/config.js
+++ config/config.js
@@ -4,8 +4,8 @@ module.exports = {
     userRoles: ['guest', 'user', 'admin'],
     accountKitUrl: 'https://graph.accountkit.com/v1.2/access_token',
     accessToken: '6f963fe06ebc888cdd74d55fd159f6ab',
-    oneSignalKey: 'N2VhNmYwZDUtMDMyMC00MDIyLWE5YzMtYTM1MDBmYzQyZDQ0',
-    oneSignalAppId: '0aefe444-ad09-494a-b457-2d9febdbf78e',
+    oneSignalKey: 'ZTM2ODljOTktYjk0My00MTFiLWI5NjYtMjM2ZTdhNmQxOWUw',
+    oneSignalAppId: '94389cce-e89a-42ed-8e6e-56011479399b',
     accountSid: 'AC81153fd1c273e81456da3a41cdf8749a',
     authToken: '3c27528a9e96fe00a470c9fcea636d12',
     yourPhone: '+16502032489'
